8c2657982592bdc945dcef8fde632e4954c384e0
jpdeleon <jpdeleon.bsap@gmail.com>
2019-08-07 18:36:21 +0900

    small setup.py fix

--- tql/__init__.py
+++ tql/__init__.py
@@ -4,4 +4,4 @@ __version__ = "0.1"
 
 from .tql import generate_QL, generate_all_lc, generate_FOV, collate_tls_results, query_toi
 
-__all__ = ["generate_QL", "generate_all_lc", "generate_FOV", "collate_tls_results", "query_toi"]
+__all__ = ["generate_QL", "generate_all_lc", "generate_FOV", "generate_multi_aperture_lc,",  "collate_tls_results", "query_toi"]
