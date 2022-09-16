32442e306e6abf9c226f5c1ea201bc142007b36f
jpdeleon <jpdeleon.bsap@gmail.com>
2022-09-16 11:33:46 +0900

    remove files

--- tests/Binks2020/mbf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env/sh
-ifp='binks2020'
-ofp=$ifp'_sc'
-sr=30
-#cat $ifp.txt | while read name; do echo tql -name \'$name\' -v -c long -lc qlp -sr $sr -s -img -f; done > $ofp.batch
-cat $ifp.txt | while read name; do echo tql -name \'$name\' -v -sr $sr -s -img -f; done > $ofp.batch
-echo 'check: cat '$ofp'.batch'
-echo 'test: cat '$ofp'.batch | sed -n 1p | sh'
-echo 'run: cat '$ofp'.batch | parallel 2>&1 | tee ' $ofp'.log'
